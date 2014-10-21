493df9ddab6907e32c283d95366e5a11886d9b6c
Matheus Dal Mago <matheusdalmago10@hotmail.com>
2014-10-21 01:11:28 -0400

    adding Account

---
+++ src/oop/hw6/TestAccount.java
@@ -0,0 +1,25 @@
+/*
+ * TestAccount.java
+ * Copyright (C) 2014 Matheus Dal Mago <matheusdalmago10@hotmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+public class TestAccount
+{
+	public static void main(String[] args) {
+        Account ac1 = new Account(1122, 20000);
+
+        ac1.setAnnualInterestRate(4.5);
+
+        ac1.withDraw(2500);
+
+        ac1.deposit(3000);
+
+        System.out.println("Balance: "+ac1.getBalance());
+        System.out.println("Monthly Interest Rate: "+
+                ac1.getMonthlyInterestRate());
+        System.out.println("Account created on: "+ac1.getDateCreated());
+	}
+}
+
